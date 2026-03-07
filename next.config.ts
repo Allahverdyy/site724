62eb08559151a48a0836f73325f3dbf77e42b0ac
ABDULSAMED <87281345+Allahverdyy@users.noreply.github.com>
2026-03-07 16:05:35 +0300

    Configure basePath and assetPrefix for production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repo = "site724"; // GitHub repo adı
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
